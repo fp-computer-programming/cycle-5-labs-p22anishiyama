9991a6fe4d5d16132c258c1110555ad41091e96c
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-10-26 10:10:45 -0400

    add lab4_1.py

---
+++ lab4_1.py
@@ -0,0 +1,20 @@
+# Author: ATN 10/26/21
+
+str1 = "Fairfield "
+str2 = "Prep"
+
+str3 = str1 + str2
+print(str3)
+
+dashes = "-"
+print(dashes * 20)
+
+story = "I\'m loving this short story I\'m reading, \"The Fall of the House of\
+ Usher\""
+print(story)
+
+print(len(story))
+
+apple = "apple"
+letter = apple[3]
+print(letter)
