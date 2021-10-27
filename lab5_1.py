69f699abc864953e1be8b65f7ff9ae2b3838e39c
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-10-27 10:20:29 -0400

    add lab5_1.py and lab5_2.py

---
+++ lab5_1.py
@@ -0,0 +1,9 @@
+# Author: ATN 10/27/21
+
+school = "Fairfield Prep"
+
+first_half = school[:9]
+second_half = school[10:]
+
+print(first_half + "\n" + second_half)
+print(first_half + " " + second_half)
---
+++ lab5_2.py
@@ -0,0 +1,11 @@
+# Author: ATN 10/27/21
+
+str1 = input("Please enter a string: ")
+str2 = input("Please enter another string: ")
+
+if (str1 == str2):
+    print("The strings are equal")
+elif (str1 > str2):
+    print("The first string is greater than the second")
+elif (str1 < str2):
+    print("The second string is greater than the first")
