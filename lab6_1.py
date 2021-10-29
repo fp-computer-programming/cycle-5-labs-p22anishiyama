65b2f2b9de431013bea14593024a13dcca671c69
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-10-29 10:44:26 -0400

    add lab6_1.py and lab6_2.py

---
+++ lab6_1.py
@@ -0,0 +1,7 @@
+# Author: ATN 10/29/21
+
+word = "flibbertigibbet"
+print(word.find("t"))
+
+wish = "I wish, I wish, I was a fish.".split(sep=",")
+print(wish)
---
+++ lab6_2.py
@@ -0,0 +1,7 @@
+# Author: ATN 10/29/21
+
+person1 = input("What is your first name? ")
+person2 = input("What is your first name? ")
+
+response = "Hello, {0}. My name is {1}.".format(person1, person2)
+print(response)
